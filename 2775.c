f473598b1ccad98288d38c2d06a9f0a0baa59bc0
HYEONSIK OH <ks06891@naver.com>
2022-04-01 23:49:58 +0900

    0401 Beakjoon

--- 2775.c
+++ 2775.c
@@ -1,19 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 	int num;
 	scanf("%d", &num);
-	int a[14][14];
+	int a[17][17];
 	int* arr = (int*)malloc(sizeof(int) * num);
 
-	for (int i = 0; i < 14; i++) {
-		a[0][i] = i+1;
+	for (int i = 0; i < 17; i++) {
+		a[0][i] = i + 1;
 		a[i][0] = 1;
 	}
 
-	for (int i = 1; i < 14; i++) 
-		for (int j = 1; j < 14; j++)
+	for (int i = 1; i < 17; i++)
+		for (int j = 1; j < 17; j++)
 			a[i][j] = a[i - 1][j] + a[i][j - 1];
 
 	for (int i = 0; i < num; i++) {
@@ -22,11 +22,10 @@ int main(void) {
 		scanf("%d", &n);
 		arr[i] = a[k][n - 1];
 	}
-	
+
 	for (int i = 0; i < num; i++)
 		printf("%d\n", arr[i]);
 
-		
-
-	
+	free(arr);
+	return 0;
 }
---
+++ 2839.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d", &num);
+	int sum;
+	int ij = 100001;
+	int k = 100000;
+
+	for (int i = 0; i < 1890; i++) {
+		for (int j = 0; j < 1001; j++) {
+			sum = 3 * i + 5 * j;
+			if (sum == num)
+				ij = i + j;
+
+			if (k > ij)
+				k = ij;
+		}
+	}
+
+	if (k == 100000)
+		printf("-1");
+	else
+		printf("%d", k);
+}
