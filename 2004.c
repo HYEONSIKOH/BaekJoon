78dddc5804d8617307120353579c0b14e2fdd776
HYEONSIK OH <ks06891@naver.com>
2022-05-08 13:35:15 +0900

    0508 BaekJoon

--- 2004.c
+++ 2004.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int count_n2(int num) {
+
+int count_n2(int num) { // 2의 개수 구하기
 	int count = 0;
 
 	while (num >= 2) {
@@ -10,7 +11,7 @@ int count_n2(int num) {
 	return count;
 }
 
-int count_n5(int num) {
+int count_n5(int num) { // 5의 개수 구하기
 	int count = 0;
 
 	while (num >= 5) {
@@ -28,10 +29,10 @@ int main(void) {
 	int count_2 = 0;
 	int count_0 = 0;
 
-	count_5 = count_n5(a) - (count_n5(b) + count_n5(a - b));
-	count_2 = count_n2(a) - (count_n2(b) + count_n2(a - b));
+	count_5 = count_n5(a) - (count_n5(b) + count_n5(a - b)); // 분자의 5의 개수 - 분모의 5의 개수
+	count_2 = count_n2(a) - (count_n2(b) + count_n2(a - b)); // 분자의 2의 개수 - 분모의 5의 개수
 	
-	while (count_5 != 0 && count_2 != 0) {
+	while (count_5 != 0 && count_2 != 0) { // 2의 개수와 5의 개수가 짝을 이룰 때, 0의 개수가 한개씩 늘어난다
 		count_0++;
 		count_5--;
 		count_2--;
